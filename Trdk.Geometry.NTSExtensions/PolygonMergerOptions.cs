00c6117139297a17922386da4530e6f72b0aa4e1
teradake <teradake@kajima.com>
2025-08-06 16:03:25 +0900

    add mergeroption

---
+++ Trdk.Geometry.NTSExtensions/PolygonMergerOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Trdk.Geometry.NTS
+{
+    public static class PolygonMergerOptions
+    {
+        private static double _tolerance = 0.0;
+        public static double Tolerance
+        {
+            get => _tolerance;
+            set => _tolerance = Math.Clamp(value, 0.0, 2.0);
+        }
+    }
+}
