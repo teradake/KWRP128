0f71f1c59236715752cd3ab83a558bbcfc9c7487
kjm-teradake_kajima <teradake@kajima.com>
2025-08-20 16:47:10 +0900

    プロパティの初期値を追加し、メソッドを実装
    `ActivityWorkTimeEstimatorOption` クラスにおいて、複数のプロパティにデフォルト値を設定しました。新たに `BuildByRoller` メソッドを追加し、`RollerModel` を引数に取ることで、`ActivityWorkTimeEstimatorOption` のインスタンスを簡潔に生成できるようになりました。

--- KWRP.Avalonia.Backend/Model/Modlules/WorkTimeEstimator/ActivityWorkTimeEstimatorOption.cs
+++ KWRP.Avalonia.Backend/Model/Modlules/WorkTimeEstimator/ActivityWorkTimeEstimatorOption.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using KWRP.Avalonia.Backend.Models.Roller;
+using System.Diagnostics;
 
 namespace KWRP.Backend.Model.Modlules.WorkTimeEstimator
 {
@@ -6,45 +7,52 @@ namespace KWRP.Backend.Model.Modlules.WorkTimeEstimator
     internal sealed class ActivityWorkTimeEstimatorOption
     {
         /// <summary>折返し時間 [sec]</summary>
-        public TimeSpan MeanSwitchbackTime { get; set; }
+        public TimeSpan MeanSwitchbackTime { get; set; } = TimeSpan.FromSeconds(3.0);
 
         /// <summary>移動時間 [sec]</summary>
-        public TimeSpan MeanTravelTime { get; set; }
+        public TimeSpan MeanTravelTime { get; set; } = TimeSpan.FromSeconds(60.0);
 
         /// <summary>ちょこ踏み時間 [sec]</summary>
-        public TimeSpan MeanChokobumiTime { get; set; }
+        public TimeSpan MeanChokobumiTime { get; set; } = TimeSpan.FromSeconds(15.0);
 
         /// <summary>くびれ距離 [m]</summary>
-        public double MeanKubireLengthMeter { get; set; }
+        public double MeanKubireLengthMeter { get; set; } = 16.0;
 
         /// <summary>起振時にちょこ踏みを考慮するか</summary>
-        public bool EnableChokobumiForCompaction { get; set; }
+        public bool EnableChokobumiForCompaction { get; set; } = true;
 
         /// <summary>無起振時にちょこ踏みを考慮するか</summary>
-        public bool EnableChokobumiForNonCompaction { get; set; }
+        public bool EnableChokobumiForNonCompaction { get; set; } = true;
 
         /// <summary>起振時にくびれを考慮するか</summary>
-        public bool EnableKubireForCompaction { get; set; }
+        public bool EnableKubireForCompaction { get; set; } = true;
 
         /// <summary>無起振時にくびれを考慮するか</summary>
-        public bool EnableKubireForNonCompaction { get; set; }
+        public bool EnableKubireForNonCompaction { get; set; } = true;
 
-        /// <summary>走行距離の一覧 [m]</summary>
-        public double[] LaneLengthListMeter { get; set; } = [];
 
-        /// <summary>転圧回数</summary>
-        public int RepeatNum { get; set; }
+        /// <summary>ローラの車種が両鉄輪ならTrue</summary>
+        public bool IsTandemRoller { get; set; } = false;
 
-        /// <summary>参照速度 [km/h]</summary>
-        public double RefSpeedKmPerHour
+        /// <summary>ローラのホイールベース</summary>
+        public double WheelbaseMeter { get; set; } = 3.0;
+
+
+        public static ActivityWorkTimeEstimatorOption BuildByRoller(RollerModel roller)
         {
-            get => _refSpeed;
-            set => _refSpeed = value;
+            return new ActivityWorkTimeEstimatorOption()
+            {
+                MeanSwitchbackTime = roller.WorkTimeEstimationOptions.MeanSwitchbackTime,
+                MeanChokobumiTime = roller.WorkTimeEstimationOptions.MeanChokobumiTime,
+                MeanTravelTime = roller.WorkTimeEstimationOptions.MeanTravelTime,
+                MeanKubireLengthMeter = roller.WorkTimeEstimationOptions.MeanKubireLengthMeter,
+                EnableChokobumiForCompaction = roller.WorkTimeEstimationOptions.Compaction.EnableChokobumi,
+                EnableChokobumiForNonCompaction = roller.WorkTimeEstimationOptions.NonCompaction.EnableChokobumi,
+                EnableKubireForCompaction = roller.WorkTimeEstimationOptions.Compaction.EnableKubire,
+                EnableKubireForNonCompaction = roller.WorkTimeEstimationOptions.NonCompaction.EnableKubire,
+                IsTandemRoller = roller.MachineInfo.RollerWheelType == Avalonia.Backend.Enums.RollerWheelType.Tandem,
+                WheelbaseMeter = roller.MachineInfo.Wheelbase,
+            };
         }
-        public double RedSpeedMeterPerSec => _refSpeed * 1000.0 / 3600.0; // km/h to m/s
-        private double _refSpeed;
-
-        /// <summary>端部フラグ</summary>
-        public bool[] EdgeFlag { get; set; } = [false, false];
     }
 }
