4806afb5913ef4378282fa933ac37408ba35db82
kjm-teradake_kajima <teradake@kajima.com>
2025-08-28 11:43:28 +0900

    ver1.2.4

--- KWRP.Avalonia.Frontend/ViewModels/MainWindowViewModel.cs
+++ KWRP.Avalonia.Frontend/ViewModels/MainWindowViewModel.cs
@@ -59,7 +59,7 @@ namespace KWRP.Avalonia.Frontend.ViewModels
         }
 
         public string AppName => "区割りシステム";
-        public string Version => "1.2.3";
+        public string Version => "1.2.4";
         public string Title => $"{AppName} ver {Version}";
 
         public INotificationMessageManager Manager => _notificationStore.Manager;
