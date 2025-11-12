2e1c35a75b01db0637e85bd34970b848b12d3edf
kjm-teradake_kajima <teradake@kajima.com>
2025-11-12 13:04:20 +0900

    init localizeExtensions

---
+++ KWRP.Avalonia.Frontend/Models/Localizer/LocalizeExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia.Data;
+using Avalonia.Markup.Xaml.MarkupExtensions;
+using Avalonia.Markup.Xaml;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace KWRP.Frontend.Models.Localizer
+{
+    public class LocalizeExtension : MarkupExtension
+    {
+        public LocalizeExtension(string key)
+        {
+            Key = key;
+            Context = string.Empty;
+        }
+
+        public string Key { get; set; }
+
+        public string Context { get; set; }
+
+        [DynamicDependency(DynamicallyAccessedMemberTypes.PublicProperties, typeof(Localizer))]
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var keyToUse = Key;
+            if (!string.IsNullOrWhiteSpace(Context))
+                keyToUse = $"{Context}/{Key}";
+
+            var binding = new ReflectionBindingExtension
+            {
+                Path = $"[{keyToUse}]",
+                Mode = BindingMode.OneWay,
+                Source = Localizer.Instance,
+            };
+
+            return binding.ProvideValue(serviceProvider);
+        }
+    }
+}
